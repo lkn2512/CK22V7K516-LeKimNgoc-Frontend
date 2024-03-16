a3a206dada19e93d2b023c89c26680bd33a28992
lkn2512 <lkn25122001@gmail.com>
2024-03-16 20:01:25 +0700

    Tao trang them lien he

---
+++ src/views/ContactCreate.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>Thêm liên hệ</h1>
+        <contact-form @submit:contact="submitContact" :contact="newContact" />
+    </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+    components: {
+        ContactForm
+    },
+    data() {
+        return {
+            newContact: {
+                name: "",
+                email: "",
+                address: "",
+                phone: "",
+                favorite: false
+            }
+        };
+    },
+    methods: {
+        async submitContact(contactData) {
+            try {
+                await ContactService.create(contactData);
+                alert("Thêm thành công!.");
+                this.$router.push({ name: "contactbook" });
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
+};
+</script>
